c570cf3e3a7f91efeb9b3004a333a53606afd0df
jikor1st <jikor1st@naver.com>
2025-01-17 18:31:04 +0900

    t

--- src/app/api/slack/route.ts
+++ src/app/api/slack/route.ts
@@ -1,3 +1,6 @@
+// import { Client } from "@notionhq/client";
+// const notion = new Client({ auth: process.env.NOTION_TOKEN });
+
 export async function GET() {
   return new Response("Hello, Slack!", {
     status: 200,
